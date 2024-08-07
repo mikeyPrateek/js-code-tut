b1ea5d04f75b4bf098a6563fcceffff17b66e91f
MikeyPrateek <137617537+mikeyPrateek@users.noreply.github.com>
2024-08-07 21:38:40 +0000

    variable & datatypes

---
+++ javascript/01_variable.js
@@ -0,0 +1,14 @@
+const accountID = 987878     // Not changable
+let accountPass = 1234567   //Always use this as a variable 
+var accountCity = "Bikaner" //NOt use in industry 
+AccountTag = "maddy"
+console.table([accountID , accountPass , accountCity , AccountTag]); //that a single way to admit all variable in single way
+
+{
+    //The gap b/w bresses is called scope
+}
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/ 
